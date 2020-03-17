20d4e4420ee8ef69ebd503249ae79683b2260e03
Grace <mgracebodur@gmail.com>
2020-03-17 11:17:46 -0400

    Solved Drill # 5 URLify strings

--- Drill5.js
+++ Drill5.js
@@ -14,3 +14,18 @@
 // Input: www.thinkful.com /tauh ida parv een
 
 // Output: www.thinkful.com%20/tauh%20ida%20parv%20een
+
+function stringToURL(string) {
+    const arr = string.split('')
+
+    const newArr = arr.map((item, index) => {
+        if(item === ' ') { // if item has an empty string 
+            return arr[index] = '%20' // replace the items index with '%20'
+        }
+        return item
+    })
+    return newArr.join('')
+}
+
+console.log(stringToURL('tauhida parveen'))
+console.log(stringToURL('www.thinkful.com /tauh ida parv een'))
